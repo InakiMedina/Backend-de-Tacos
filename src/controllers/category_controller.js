a9da885f8f129aa74b77cb9a5fa4ee8d630a7d82
Cristian8a <cris_17e@hotmail.com>
2024-02-22 08:59:27 -0600

    CRUDScategories

--- src/controllers/category_controller.js
+++ src/controllers/category_controller.js
@@ -1,1 +1,65 @@
-const model = require('../models/category');
+const Category = require('../models/category.js');
+
+// Obtener todas las categorías
+const getAllCategories = async (req, res) => {
+    try {
+        const categories = await Category.find();
+        res.json(categories);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Crear una nueva categoría
+const createCategory = async (req, res) => {
+    const category = new Category({
+        name: req.body.name
+    });
+    try {
+        const newCategory = await category.save();
+        res.status(201).json(newCategory);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+// Obtener una categoría por su ID
+const getCategoryById = async (req, res) => {
+    try {
+        const category = await Category.findById(req.params.id);
+        if (category == null) {
+            return res.status(404).json({ message: 'Categoría no encontrada' });
+        }
+        res.json(category);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Actualizar una categoría por su ID
+const updateCategory = async (req, res) => {
+    try {
+        const updatedCategory = await Category.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(updatedCategory);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+// Eliminar una categoría por su ID
+const deleteCategory = async (req, res) => {
+    try {
+        await Category.findByIdAndDelete(req.params.id);
+        res.json({ message: 'Categoría eliminada' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = {
+    getAllCategories,
+    createCategory,
+    getCategoryById,
+    updateCategory,
+    deleteCategory
+};
