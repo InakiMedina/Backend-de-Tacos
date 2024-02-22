0ea8483c4dd161097f6d3cbeed44d50d662c2602
Octavio Valdez <90404916+OctavioValdez@users.noreply.github.com>
2024-02-22 09:04:44 -0600

    Tables crud (#10)

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const category = require('./category')
 const products = require('./products')
+const table = require('./table');
 const user = require('./user')
 // const path = require('path');  //NOt installed yet, maybe will use?
 
@@ -31,6 +32,6 @@ router.get('/', (req, res) => {
 router.use('', express.json());
 router.use('/category', category);
 router.use('/products', products);
+router.use('/table', table);
 router.use('/user', user);
-
 module.exports = router;
