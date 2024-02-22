923f4333e6c0af215cc4e184a94bcb5567a1cc48
ddkktt <128318872+ddkktt@users.noreply.github.com>
2024-02-22 08:27:35 -0600

    correct user routes (#7)
    * .
    
    * .
    
    * createUser
    
    * .
    
    * .
    
    * getuser controller and route
    
    * .
    
    ---------
    
    Co-authored-by: InakiMedina <83434250+InakiMedina@users.noreply.github.com>

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const userController = require('../controllers/user.js') // Controller
 
 router.get('', userController.getUsers);
-router.get('/:id', userController.createUser);
+router.get('/:id', userController.getUser);
 router.post('/create', userController.createUser);
-router.get('/:id', userController.createUser);
+
 
 module.exports = router;
