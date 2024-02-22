e6fba2f54f2643e9f115d2fa1beb46284b2db8e6
ddkktt <128318872+ddkktt@users.noreply.github.com>
2024-02-22 07:51:11 -0600

    createUser

---
+++ src/controllers/user.js
@@ -0,0 +1,32 @@
+const userModel = require('../models/user')
+
+module.exports = {
+
+    createUser : async(req,res) => {
+        console.log("here");
+        try{
+            
+            let user = {
+                firstName : req.body.firstname,
+                lastName : req.body.lastName,
+                email : req.body.email,
+                password: req.body.password,
+                type: req.body.type
+            }
+            console.log(user);
+            userModel.create(user).then(result => {
+                res.send(result);
+            }).catch(err => {
+                console.error(err);
+                res.status(500).send("Error al adding movie");
+            });
+
+        }
+        catch(err){
+            return err
+        }
+        
+    }
+
+
+}
