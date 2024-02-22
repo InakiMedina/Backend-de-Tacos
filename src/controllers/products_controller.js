1cd4db73c2d8aea7b87ff3014b703a8c00b3cc4f
ACasillas01 <acasillas00@outlook.com>
2024-02-22 08:29:20 -0600

    Imports updated + Products controller updates

--- src/controllers/products_controller.js
+++ src/controllers/products_controller.js
@@ -2,7 +2,7 @@ const model = require('../models/products');
 
 
 module.exports = {
-    find: async (req, res) => {
+    GetProducts: async (req, res) => {
         collection = req.baseUrl; // /category, /products, etc
         query = req.query;
         console.log(collection);
@@ -13,15 +13,26 @@ module.exports = {
             res.status(500).send('Not a valid URL path!');
         });
     },
-
-    find_one: async (req, res) => {
-        collection = req.baseUrl; // /category, /products, etc
-        query = req.query;
-        await model2.findOne(query).lean().then(response => {
+    GetProduct: async (req, res) => {
+        id = req.query.id;
+        await model2.findOne({
+            _id: id
+        }).lean().then(response => {
             res.send(response)
         }).catch(error => {
             console.error(error);
-            res.status(500).send('Not a valid URL path!')
+            res.status(500).send('Not a valid id!')
+        });
+    },
+    DeleteProduct: async (req, res) => {
+        let id = req.body.id;
+        await model2.findByIdAndDelete({
+            _id: id
+        }).lean().then(response => {
+            res.send(response)
+        }).catch(error => {
+            console.error(error);
+            res.status(500).send('Not a valid id!')
         });
     }
 
